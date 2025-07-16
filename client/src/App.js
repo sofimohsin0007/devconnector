49d81cde8393a8f59d6317986ee62f0ce5dc570e
mosinf <mosinf@scribd.com>
2025-07-16 19:50:56 +0530

    edit profile

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,10 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/profile-forms/CreateProfile";
+import EditProfile from "./components/profile-forms/EditProfile";
 import PrivateRoute from "./components/routing/PrivateRoute";
+
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -40,6 +43,16 @@ const App = () => {
               path="dashboard"
               element={<PrivateRoute component={Dashboard} />}
             />
+            <Route
+              path="create-profile"
+              element={<PrivateRoute component={CreateProfile} />}
+            />
+            <Route
+              path="edit-profile"
+              element={<PrivateRoute component={EditProfile} />}
+            />
+
+            {/* <Route path="create-profile" element={<CreateProfile />} /> */}
           </Routes>
         </Fragment>
       </Router>
