525bb0a1d9a6da6b01a547588e2c047d1708d494
mosinf <mosinf@scribd.com>
2025-07-22 19:17:15 +0530

    single post display

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
 import PrivateRoute from "./components/routing/PrivateRoute";
 
 // Redux
@@ -64,6 +66,11 @@ const App = () => {
             <Route
               path="add-education"
               element={<PrivateRoute component={AddEducation} />}
+            />
+            <Route path="/posts" element={<PrivateRoute component={Posts} />} />
+            <Route
+              path="/posts/:id"
+              element={<PrivateRoute component={Post} />}
             />
 
             {/* <Route path="create-profile" element={<CreateProfile />} /> */}
